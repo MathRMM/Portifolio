46dd72f9515f71c4567607a9796e878d723fcc90
Math <math@pop-os.localdomain>
2022-11-25 23:00:34 -0300

    ajust build vsConfig

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,16 @@ import path from 'path'
 export default defineConfig({
   plugins: [react()],
   root: path.resolve(__dirname, 'src'),
+  build: {
+    outDir: '../dist'
+  },
   resolve: {
     alias: {
       '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
     }
   },
   server: {
-    port: 8081,
+    port: 8080,
     hot: true
   }
 })
